451700d7c76d6835dd005d3cfbb3c35e593ed83f
David Fernandez Reboredo <davidfr2001@hotmail.com>
2024-11-27 17:06:09 +0100

    actualizar carpeta delay2

--- delay_2/delay2/delay2.ino
+++ delay_2/delay2/delay2.ino
@@ -1,9 +1,8 @@
+int ctr1=0;
+int ctr2=0;
 void setup() {
   Serial.begin(9600);
 }
-int ctr1=0;
-int ctr2=0;
-
 void loop() {
   for(int i=0;i<=20;i++){
     ctr1++;
@@ -11,8 +10,8 @@ void loop() {
     delay(100);
   }
   ctr2++;
-  mostrar(ctr1,ctr2);
   ctr1-=ctr2;
+  mostrar(ctr1,ctr2);
 }
 void mostrar(int ctr1,int ctr2){
   Serial.print("C1: ");
